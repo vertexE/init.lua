970e66865d3bc9f89deef26e2f46279a8b106e6a
Josiah Denton <jfranklindenton@gmail.com>
2025-10-24 01:06:28 -0400

    feat: tweak filepath appearance

--- lua/ui/winbar/segments/filepath.lua
+++ lua/ui/winbar/segments/filepath.lua
@@ -2,30 +2,103 @@ local M = {}
 
 local store = require("ui.winbar.store")
 
-local icons = {
-    lua = " ",
-    python = "󰌠 ",
-    typescriptreact = " ",
-    javascriptreact = " ",
-    json = " ",
-    html = " ",
-    css = " ",
-    go = " ",
-    rust = " ",
-    typescript = "󰛦 ",
-    javascript = " ",
+--- @class winbar.filetype
+--- @field ft string
+--- @field icon string
+--- @field hl string
+
+--- @type table<winbar.filetype>
+local file_type_decorations = {
+    {
+        ft = "lua",
+        icon = " ",
+        hl = "MiniIconsAzure",
+    },
+    {
+        ft = "python",
+        icon = "󰌠 ",
+        hl = "MiniIconsYellow",
+    },
+    {
+        ft = "typescriptreact",
+        icon = " ",
+        hl = "MiniIconsBlue",
+    },
+    {
+        ft = "javascriptreact",
+        icon = " ",
+        hl = "MiniIconsBlue",
+    },
+    {
+        ft = "json",
+        icon = " ",
+        hl = "MiniIconsYellow",
+    },
+    {
+        ft = "html",
+        icon = " ",
+        hl = "MiniIconsRed",
+    },
+    {
+        ft = "css",
+        icon = " ",
+        hl = "MiniIconsBlue",
+    },
+    {
+        ft = "go",
+        icon = " ",
+        hl = "MiniIconsCyan",
+    },
+    {
+        ft = "rust",
+        icon = " ",
+        hl = "MiniIconsOrange",
+    },
+    {
+        ft = "typescript",
+        icon = "󰛦 ",
+        hl = "MiniIconsBlue",
+    },
+    {
+        ft = "javascript",
+        icon = " ",
+        hl = "MiniIconsYellow",
+    },
+    {
+        ft = "markdown",
+        icon = " ",
+        hl = "MiniIconsYellow",
+    },
 }
 
---- @return string
+--- @return table<table<string,string>>
 local file_path = function()
     local bufnr = vim.api.nvim_get_current_buf()
     local path = vim.fn.expand("%:.")
     local file = vim.fn.fnamemodify(path, ":t")
     local ext = vim.bo[bufnr].filetype
     if #file == 0 then
-        return ""
+        return {}
     end
-    return (icons[ext] or "") .. file .. (vim.bo.modified and " ● " or "   ")
+    --- @type winbar.filetype
+    local ft_decoration = vim.iter(file_type_decorations):find(function(decoration)
+        return decoration.ft == ext
+    end)
+
+    if not ft_decoration then
+        ft_decoration = {
+            ft = "unknown",
+            icon = " ",
+            hl = "MiniIconsGray",
+        }
+    end
+
+    return {
+        { "│ " or "", "Comment" },
+        { ft_decoration.icon, ft_decoration.hl },
+        { file, "@constant" },
+        { vim.bo.modified and " ● " or "   ", "Comment" },
+    }
 end
 
 M.setup = function()
@@ -33,20 +106,10 @@ M.setup = function()
         name = "filepath",
         split = true,
         focused = function()
-            local file = file_path()
-            return {
-                { #file > 0 and "│ " or "", "Comment" },
-                { file, "@text" },
-                { "", "Comment" },
-            }
+            return file_path()
         end,
         default = function()
-            local file = file_path()
-            return {
-                { #file > 0 and "│ " or "", "Comment" },
-                { file, "@text" },
-                { "", "Comment" },
-            }
+            return file_path()
         end,
     })
 end
