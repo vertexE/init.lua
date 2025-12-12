8dfa43f04edb2cb7f538b0ec1fe6efe96b20c2ac
Josiah Denton <jfranklindenton@gmail.com>
2025-12-12 11:29:13 -0500

    chore: rmv lsp signs

--- lua/lsp/diagnostics.lua
+++ lua/lsp/diagnostics.lua
@@ -3,24 +3,10 @@ local M = {}
 local custom_diagnostics = require("ui.diagnostics")
 
 M.setup = function()
-    local signs = require("symbols").lsp_signs()
-    for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
-
     custom_diagnostics.setup()
 
     vim.diagnostic.config({
-        signs = {
-            enable = true,
-            text = {
-                ["ERROR"] = signs.Error,
-                ["WARN"] = signs.Warn,
-                ["HINT"] = signs.Hint,
-                ["INFO"] = signs.Info,
-            },
-        },
+        signs = false,
         virtual_text = false,
         virtual_lines = false,
         inlay_hint = {
