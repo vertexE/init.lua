e6bc3990cd2d3aa5bf99062b12719a1e84ce3de3
Josiah Denton <jfranklindenton@gmail.com>
2026-04-26 15:27:04 -0400

    feat: spinning UI for loader

--- lua/ui/loader.lua
+++ lua/ui/loader.lua
@@ -1,11 +1,18 @@
 local M = {}
 
+--- @class ui.loader
+--- @field bufnr integer
+--- @field extmark_id integer
+--- @field frame integer
+
 --- namespace ID  --> bufnr
---- @type table<integer,integer>
+--- @type table<integer,ui.loader>
 local active_loaders = {}
 
 --- @type table<integer,integer>
-local ns_to_extmark_id = {}
+-- local ns_to_extmark_id = {}
+
+local counter = 0
 
 --- dist to first non whitespace char
 --- @param s ?string
@@ -19,31 +26,69 @@ local dist_to_non_whitespace = function(s)
     return _end and _end or 0
 end
 
-M.start = function(bufnr, start_row, end_row, apply_ghost)
-    local loader_id = #active_loaders
-    local ns_id = vim.api.nvim_create_namespace("user_loader_vt_" .. loader_id)
-    local lines = vim.api.nvim_buf_get_lines(bufnr, start_row - 1, end_row, false)
-    local ext_id = vim.api.nvim_buf_set_extmark(bufnr, ns_id, start_row - 1, 0, {
-        virt_lines = {
-            {
-                { string.rep(" ", dist_to_non_whitespace(lines[1])), "Comment" },
-                { "Thinking...", "DiagnosticOk" },
-            },
-        },
-        virt_lines_above = true,
-    })
-    ns_to_extmark_id[ns_id] = ext_id
+local frames = {
+    "⣾",
+    "⣽",
+    "⣻",
+    "⢿",
+    "⡿",
+    "⣟",
+    "⣯",
+    "⣷",
+}
 
-    if apply_ghost then
-        for i, line in ipairs(lines) do
-            vim.api.nvim_buf_set_extmark(bufnr, ns_id, start_row - 1 + i - 1, 0, {
-                hl_group = "Comment",
-                end_col = #line,
+local draw_loop = function(bufnr, ns_id, start_row, line)
+    local timer = vim.loop.new_timer()
+    timer:start(
+        0,
+        100,
+        vim.schedule_wrap(function()
+            local row = M.location(bufnr, ns_id)
+            if not active_loaders[ns_id] then
+                timer:stop()
+                timer:close()
+                return
+            end
+
+            vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
+            local ext_id = vim.api.nvim_buf_set_extmark(bufnr, ns_id, row >= 0 and row or (start_row - 1), 0, {
+                virt_lines = {
+                    {
+                        { string.rep(" ", dist_to_non_whitespace(line)), "Comment" },
+                        {
+                            string.format("Thinking %s", frames[active_loaders[ns_id].frame % #frames + 1]),
+                            "DiagnosticOk",
+                        },
+                    },
+                },
+                virt_lines_above = true,
             })
-        end
-    end
+            active_loaders[ns_id].frame = active_loaders[ns_id].frame + 1
+            active_loaders[ns_id].extmark_id = ext_id
+        end)
+    )
+end
 
-    active_loaders[ns_id] = bufnr
+M.start = function(bufnr, start_row, end_row, apply_ghost)
+    local ns_id = vim.api.nvim_create_namespace("user_loader_vt_" .. counter)
+    counter = counter + 1
+    active_loaders[ns_id] = {
+        bufnr = bufnr,
+        extmark_id = -1, -- set in schedule_draw
+        frame = 1,
+    }
+    local lines = vim.api.nvim_buf_get_lines(bufnr, start_row - 1, end_row, false)
+    draw_loop(bufnr, ns_id, start_row, lines[1])
+
+    -- TODO: refactor ghost
+    -- if apply_ghost then
+    --     for i, line in ipairs(lines) do
+    --         vim.api.nvim_buf_set_extmark(bufnr, ns_id, start_row - 1 + i - 1, 0, {
+    --             hl_group = "Comment",
+    --             end_col = #line,
+    --         })
+    --     end
+    -- end
 
     return ns_id
 end
@@ -51,12 +96,11 @@ end
 --- get the line number of an active loader
 --- @return integer the line number, 0-indexed
 M.location = function(buf, ns)
-    if ns_to_extmark_id[ns] == nil then
-        vim.notify("(loader): failed to lookup extmark ID by ns ID", vim.log.levels.ERROR)
+    if not active_loaders[ns] or active_loaders[ns].extmark_id < 0 then
         return -1
     end
 
-    local row = vim.api.nvim_buf_get_extmark_by_id(buf, ns, ns_to_extmark_id[ns], {})[1]
+    local row = vim.api.nvim_buf_get_extmark_by_id(buf, ns, active_loaders[ns].extmark_id, {})[1]
     return row
 end
 
@@ -66,7 +110,8 @@ M.stop = function(ns)
         return
     end
 
-    vim.api.nvim_buf_clear_namespace(active_loaders[ns], ns, 0, -1)
+    vim.api.nvim_buf_clear_namespace(active_loaders[ns].bufnr, ns, 0, -1)
+    active_loaders[ns] = nil
 end
 
 return M
